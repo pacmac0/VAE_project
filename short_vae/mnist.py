02f7598537c24baccaa285a98ea03c3c8a6e4fb2
sam hedin <sam.hedin@gmail.com>
2020-12-31 10:13:09 +0100

    refactoring

--- short_vae/mnist.py
+++ short_vae/mnist.py
@@ -1,100 +1,31 @@
 #!/usr/bin/env python3
 import torch
 import torchvision
-import torch.optim as optim
 import argparse
 import matplotlib
 import torch.nn as nn
 import matplotlib.pyplot as plt
-import torchvision.transforms as transforms
-import model
-from tqdm import tqdm
+from model import run
 from torchvision import datasets
 from torch.utils.data import DataLoader
-from torchvision.utils import save_image
+import torchvision.transforms as transforms
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-transform = transforms.Compose([transforms.ToTensor()])
-
-train_data = datasets.MNIST(
-    root="../input/data", train=True, download=True, transform=transform
-)
-val_data = datasets.MNIST(
-    root="../input/data", train=False, download=True, transform=transform
-)
+torch.set_num_threads(8)
 
 epochs = 20
 batch_size = 64
 lr = 0.0001
-train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
-
-model = model.LinearVAE().to(device)
-optimizer = optim.Adam(model.parameters(), lr=lr)
-criterion = nn.BCELoss(reduction="sum")
 
 
-def fit(model, dataloader):
-    model.train()
-    running_loss = 0.0
-    for i, data in tqdm(
-        enumerate(dataloader), total=int(len(train_data) / dataloader.batch_size)
-    ):
-        data, _ = data
-        data = data.to(device)
-        data = data.view(data.size(0), -1)
-        optimizer.zero_grad()
-        reconstruction, mu, logvar = model(data)
-        bce_loss = criterion(reconstruction, data)
-        loss = final_loss(bce_loss, mu, logvar)
-        running_loss += loss.item()
-        loss.backward()
-        optimizer.step()
-    train_loss = running_loss / len(dataloader.dataset)
-    return train_loss
+transform = transforms.Compose([transforms.ToTensor()])
+train_data = datasets.MNIST(
+    root="../input/data", train=True, download=True, transform=transform
+)
+
+val_data = datasets.MNIST(
+    root="../input/data", train=False, download=True, transform=transform
+)
 
 
-def validate(model, dataloader):
-    model.eval()
-    running_loss = 0.0
-    with torch.no_grad():
-        for i, data in tqdm(
-            enumerate(dataloader), total=int(len(val_data) / dataloader.batch_size)
-        ):
-            data, _ = data
-            data = data.to(device)
-            data = data.view(data.size(0), -1)
-            reconstruction, mu, logvar = model(data)
-            bce_loss = criterion(reconstruction, data)
-            loss = final_loss(bce_loss, mu, logvar)
-            running_loss += loss.item()
 
-            # save the last batch input and output of every epoch
-            if i == int(len(val_data) / dataloader.batch_size) - 1:
-                num_rows = 8
-                both = torch.cat(
-                    (
-                        data.view(batch_size, 1, 28, 28)[:8],
-                        reconstruction.view(batch_size, 1, 28, 28)[:8],
-                    )
-                )
-                save_image(both.cpu(), f"outputs/output{epoch}.png", nrow=num_rows)
-    val_loss = running_loss / len(dataloader.dataset)
-    return val_loss
-
-
-def final_loss(bce_loss, mu, logvar):
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    return bce_loss + KLD
-
-
-train_loss = []
-val_loss = []
-for epoch in range(epochs):
-    print(f"Epoch {epoch+1} of {epochs}")
-    train_epoch_loss = fit(model, train_loader)
-    val_epoch_loss = validate(model, val_loader)
-    train_loss.append(train_epoch_loss)
-    val_loss.append(val_epoch_loss)
-    print(f"Train Loss: {train_epoch_loss:.4f}")
-    print(f"Val Loss: {val_epoch_loss:.4f}")
+run(epochs, batch_size, lr, train_data, val_data)
--- short_vae/model.py
+++ short_vae/model.py
@@ -1,7 +1,13 @@
 import torch
+import torchvision.transforms as transforms
+import torch.optim as optim
+from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets
+from torchvision.utils import save_image
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 features = 16
 # define a simple linear VAE
@@ -44,3 +50,69 @@ class LinearVAE(nn.Module):
 
 
 
+def fit(model, dataloader, optimizer, criterion):
+    model.train()
+    running_loss = 0.0
+    for i, data in enumerate(dataloader):
+        data, _ = data
+        data = data.to(device)
+        data = data.view(data.size(0), -1)
+        optimizer.zero_grad()
+        reconstruction, mu, logvar = model(data)
+        bce_loss = criterion(reconstruction, data)
+        loss = final_loss(bce_loss, mu, logvar)
+        running_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+    train_loss = running_loss / len(dataloader.dataset)
+    return train_loss
+
+
+def validate(model, dataloader, batch_size, criterion, val_data, epoch):
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for i, data in enumerate(dataloader):
+            data, _ = data
+            data = data.to(device)
+            data = data.view(data.size(0), -1)
+            reconstruction, mu, logvar = model(data)
+            bce_loss = criterion(reconstruction, data)
+            loss = final_loss(bce_loss, mu, logvar)
+            running_loss += loss.item()
+
+            # save the last batch input and output of every epoch
+            if i == int(len(val_data) / dataloader.batch_size) - 1:
+                num_rows = 8
+                both = torch.cat(
+                    (
+                        data.view(batch_size, 1, 28, 28)[:8],
+                        reconstruction.view(batch_size, 1, 28, 28)[:8],
+                    )
+                )
+                save_image(both.cpu(), f"outputs/output{epoch}.png", nrow=num_rows)
+    val_loss = running_loss / len(dataloader.dataset)
+    return val_loss
+
+
+def final_loss(bce_loss, mu, logvar):
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return bce_loss + KLD
+
+def run(epochs, batch_size, lr, train_data, val_data):
+    model = LinearVAE().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criterion = nn.BCELoss(reduction="sum")
+
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
+    train_loss = []
+    val_loss = []
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1} of {epochs}")
+        train_epoch_loss = fit(model, train_loader, optimizer, criterion)
+        val_epoch_loss = validate(model, val_loader, batch_size, criterion, val_data, epoch)
+        train_loss.append(train_epoch_loss)
+        val_loss.append(val_epoch_loss)
+        print(f"Train Loss: {train_epoch_loss:.4f}")
+        print(f"Val Loss: {val_epoch_loss:.4f}")
