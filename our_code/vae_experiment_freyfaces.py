e7db40f0221d434b244d72a60a79d9675950c3cd
Sam Hedin <sam.hedin@gmail.com>
2021-01-04 16:40:18 +0100

    freyfaces

--- our_code/vae_experiment_freyfaces.py
+++ our_code/vae_experiment_freyfaces.py
@@ -2,6 +2,39 @@
 from scipy.io import loadmat
 import torch
 import numpy as np
+from VAE import VAE, training, testing
+
+config = {
+    #"seed": 14,
+    #"dataset_name": "static_mnist",
+    #"model_name": "vae",
+    "prior": "standard", #"vamp", # standard
+    "pseudo_components": 500,
+    "warmup": 100,
+    "z1_size": 40,
+    #"z2_size": 40,
+    "batch_size": 100,
+    "test_batch_size": 100,
+    "input_size": [1, 28, 28],
+    "input_type": "binary",
+    #"dynamic_binarization": False,
+    #"use_training_data_init": 1,
+    #"pseudoinputs_std": 0.01,
+    #"pseudoinputs_mean": 0.05,
+    "learning_rate": 0.0005,
+    "max_epoch": 2000,
+    "file_name_model": "./snapshots/model.model",
+}
+
+if torch.cuda.is_available():
+    dev = 'cuda'
+    print("--> Using GPU Cuda")
+else:
+    dev = 'cpu'
+    torch.set_num_threads(8) # threading on cpu only
+    print("--> Using CPU")
+
+device = torch.device(dev)
 
 # DOWNLOAD FROM HERE: http://www.cs.nyu.edu/~roweis/data/frey_rawface.mat
 if __name__ == "__main__":
@@ -21,4 +54,10 @@ if __name__ == "__main__":
     train_loader = torch.utils.data.DataLoader(train, batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val, batch_size, shuffle=True)
 
-    train_loader, val_loader
+    model = VAE(config)
+    model.to(device)
+
+    training(model, train_loader, config["max_epoch"], config["warmup"], config["file_name_model"],
+             config["learning_rate"])
+
+    testing(model, None, val_loader)
