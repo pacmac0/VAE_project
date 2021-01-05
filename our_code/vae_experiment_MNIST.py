3b0be95810627ff5c8add5feb9524899c579c2f1
sam hedin <sam.hedin@gmail.com>
2021-01-05 10:10:18 +0100

    Revert "Refactoring, move training and testing to mnist"
    This reverts commit c86182af19e22d89e8e5daf05cd313fba7bc2632.

--- our_code/vae_experiment_MNIST.py
+++ our_code/vae_experiment_MNIST.py
@@ -9,53 +9,57 @@ import torch.nn as nn
 import time
 
 import torch.utils.data as data_utils
-from VAE import VAE
+from VAE import VAE, training, testing
 
 config = {
-    "prior": "vamp",  # standard
+    #"seed": 14,
+    #"dataset_name": "static_mnist",
+    #"model_name": "vae",
+    "prior": "vamp", # standard
     "pseudo_components": 500,
     "warmup": 100,
     "z1_size": 40,
+    #"z2_size": 40,
     "batch_size": 100,
     "test_batch_size": 100,
     "input_size": [1, 28, 28],
     "input_type": "binary",
+    #"dynamic_binarization": False,
+    #"use_training_data_init": 1,
+    #"pseudoinputs_std": 0.01,
+    #"pseudoinputs_mean": 0.05,
     "learning_rate": 0.0005,
-    "epochs": 2000,
-    "pseudo_from_data": True,
+    "max_epoch": 2000,
+    "file_name_model": "./snapshots/model.model",
+    'pseudo_from_data': True,
 }
 
 # use GPU
 if torch.cuda.is_available():
-    dev = "cuda"
-    print("Cuda")
+    dev = 'cuda'
+    print("--> Using GPU Cuda")
 else:
-    dev = "cpu"
-    torch.set_num_threads(8)  # threading on cpu only
-    print("CPU")
+    dev = 'cpu'
+    torch.set_num_threads(8) # threading on cpu only
+    print("--> Using CPU")
 
-device = torch.device(dev)
-
+device = torch.device(dev) 
 
 def load_static_mnist(args):
     args["input_size"] = [1, 28, 28]
     args["input_type"] = "binary"
-    # args["dynamic_binarization"] = False
+    #args["dynamic_binarization"] = False
     # load each file separate
     with open(
         os.path.join("datasets", "MNIST_static", "binarized_mnist_train.amat")
     ) as f:
         lines = f.readlines()
-    train_data = np.array([[int(i) for i in l.split()] for l in lines]).astype(
-        "float32"
-    )
+    train_data = np.array([[int(i) for i in l.split()] for l in lines]).astype("float32")
     with open(
         os.path.join("datasets", "MNIST_static", "binarized_mnist_valid.amat")
     ) as f:
         lines = f.readlines()
-    evaluation_data = np.array([[int(i) for i in l.split()] for l in lines]).astype(
-        "float32"
-    )
+    evaluation_data = np.array([[int(i) for i in l.split()] for l in lines]).astype("float32")
     with open(
         os.path.join("datasets", "MNIST_static", "binarized_mnist_test.amat")
     ) as f:
@@ -68,47 +72,28 @@ def load_static_mnist(args):
 
     # pytorch data loader to create tensors
     train_loader = data_utils.DataLoader(
-        data_utils.TensorDataset(
-            torch.from_numpy(train_data), torch.from_numpy(train_labels)
-        ),
-        batch_size=args["batch_size"],
-        shuffle=True,
-    )
+        data_utils.TensorDataset(torch.from_numpy(train_data), torch.from_numpy(train_labels)), 
+        batch_size=args["batch_size"], shuffle=True)
 
     eval_loader = data_utils.DataLoader(
-        data_utils.TensorDataset(
-            torch.from_numpy(evaluation_data), torch.from_numpy(evaluation_labels)
-        ),
-        batch_size=args["test_batch_size"],
-        shuffle=False,
-    )
+        data_utils.TensorDataset(torch.from_numpy(evaluation_data), torch.from_numpy(evaluation_labels)), 
+        batch_size=args["test_batch_size"], shuffle=False)
 
     test_loader = data_utils.DataLoader(
-        data_utils.TensorDataset(
-            torch.from_numpy(test_data), torch.from_numpy(test_labels)
-        ),
-        batch_size=args["test_batch_size"],
-        shuffle=True,
-    )
+        data_utils.TensorDataset(torch.from_numpy(test_data), torch.from_numpy(test_labels)), 
+        batch_size=args["test_batch_size"], shuffle=True)
 
     # get pseudo init params from random data
-    if args["pseudo_from_data"] == True:
-        args["pseudo_std"] = 0.01
+    if args['pseudo_from_data'] == True:
+        args['pseudo_std'] = 0.01
         np.random.shuffle(train_data)
-        # print("DIM: {}".format(train_data.shape))
-        dat = train_data[
-            0 : int(args["pseudo_components"])
-        ].T  # make columns components(data-points)
-        # print("DIM: {}".format(dat.shape))
-        rand_std_norm = np.random.randn(
-            np.prod(args["input_size"]), args["pseudo_components"]
-        )
-        args["pseudo_mean"] = torch.from_numpy(
-            dat + args["pseudo_std"] * rand_std_norm
-        ).float()
-
+        #print("DIM: {}".format(train_data.shape))
+        dat = train_data[0 : int(args['pseudo_components']) ].T # make columns components(data-points)
+        #print("DIM: {}".format(dat.shape))
+        rand_std_norm = np.random.randn(np.prod(args['input_size']), args['pseudo_components'])
+        args['pseudo_mean'] = torch.from_numpy(dat + args['pseudo_std'] * rand_std_norm).float()
+        
     return train_loader, eval_loader, test_loader, args
-
 
 # usage from main: plot_tensor(inputs[0])
 def plot_tensor(tensor):
@@ -119,113 +104,49 @@ def plot_tensor(tensor):
     plt.show()
 
 
-def training(model, train_loader, epochs, warmup_period, learning_rate=0.0005):
-    model.train()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    for epoch in range(1, epochs + 1):
-        # Warm up
-        # https://arxiv.org/abs/1511.06349 [5], KL cost annealing, ch.3.
-        beta = 1.0 * epoch / warmup_period
-        if beta > 1.0:
-            beta = 1.0
-        print(f"--> beta: {beta}")
-
-        train_loss = []
-        train_re = []
-        train_kl = []
-
-        start_epoch_time = time.time()
-
-        for i, (inputs, _) in enumerate(train_loader):
-            optimizer.zero_grad()
-            # print("\nTraining batch #", i)
-
-            inputs = inputs.to(device)
-
-            # forward
-            mean_dec, logvar_dec, z, mean_enc, logvar_enc = model.forward(inputs)
-
-            # calculate loss
-            loss, RE, KL = model.get_loss(
-                inputs, mean_dec, z, mean_enc, logvar_enc, beta=beta
-            )
-
-            # backpropagate
-            loss.backward()
-
-            if i == len(train_loader) / 2:
-                print("loss", loss.item(), "RE", RE.item(), "KL", KL.item())
-
-            optimizer.step()
-
-            # collect epoch statistics
-            train_loss.append(loss.item())
-            train_re.append(RE.item())
-            train_kl.append(KL.item())
-
-        epoch_loss = sum(train_loss) / len(train_loader)
-        epoch_re = sum(train_re) / len(train_loader)
-        epoch_kl = sum(train_kl) / len(train_loader)
-
-        end_epoch_time = time.time()
-        epoch_time_diff = end_epoch_time - start_epoch_time
-
-        print(
-            f"Epoch: {epoch}; loss: {epoch_loss}, RE: {epoch_re}, KL: {epoch_kl}, time elapsed: {epoch_time_diff}"
-        )
-
-        # save parameters
-        with open("./snapshots/mnist_model", "wb") as f:
-            torch.save(model, f)
-
-
-def testing(model, train_loader, test_loader):
-    test_loss = []
-    test_re = []
-    test_kl = []
-
-    # evaulation mode
-    model.eval()
-
-    for (inputs, _) in test_loader:
-        # get input, data as the list of [inputs, label]
-        inputs = inputs.to(device)
-
-        mean_dec, logvar_dec, z, mean_enc, logvar_enc = model.forward(inputs)
-        loss, RE, KL = model.get_loss(
-            inputs, mean_dec, z, mean_enc, logvar_enc, beta=1.0
-        )
-
-        test_loss.append(loss.item())
-        test_re.append(RE.item())
-        test_kl.append(KL.item())
-
-    mean_loss = sum(test_loss) / len(test_loader)
-    mean_re = sum(test_re) / len(test_loader)
-    mean_kl = sum(test_kl) / len(test_loader)
-
-    print(f"Test results: loss avg: {mean_loss}, RE avg: {mean_re}, KL: {mean_kl}")
-
-
-if __name__ == "__main__":
+def main(args):
     torch.manual_seed(14)
-    # TODO: refactor load_static_mnist
-    train_loader, eval_loader, test_loader, args = load_static_mnist(config)
+    print(args)
 
-    model = VAE(args)
+    # TODO: refactor load_static_mnist
+    print("--> Loading data... ")
+    train_loader, eval_loader, test_loader, args = load_static_mnist(args)
+
+    # If a snapshot exist in /snapshots then use trained weights
+    file_name = args["file_name_model"]
+    if osp.exists(file_name):
+        with open(file_name, "rb") as f:
+            model = torch.load(f)
+        print("--> Loaded from pretrained model")
+    else:  # Otherwise create and intialize a new model
+        model = VAE(args)
+        print("--> Initialized new model")
     model.to(device)
 
     print("--> Starting training")
     start_time = time.time()
 
-    epochs = args["epochs"]
+    max_epoch = args["max_epoch"]
     warmup = args["warmup"]
     learning_rate = args["learning_rate"]
-    training(model, train_loader, epochs, warmup, learning_rate)
+    training(
+        model,
+        train_loader, 
+        max_epoch, 
+        warmup, 
+        file_name, 
+        learning_rate
+    )
     end_time = time.time()
     time_diff = end_time - start_time
     print("--> Training done, time elapsed: ", time_diff)
     print("--> Testing on test data")
-    testing(model, train_loader, test_loader)
+    testing(
+        model,
+        train_loader, 
+        test_loader
+    )
     print("--> Finito")
+    
+if __name__ == "__main__":
+    main(config)
