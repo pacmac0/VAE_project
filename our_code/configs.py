19cb3c30adb30b4d8b7b948ad0afa135684995e9
sam hedin <sam.hedin@gmail.com>
2021-01-07 17:38:55 +0100

    different config for different priors

---
+++ our_code/configs.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+import torch
+
+if torch.cuda.is_available():
+    dev = "cuda"
+    print("Using GPU Cuda")
+else:
+    dev = "cpu"
+    #torch.set_num_threads(len(os.sched_getaffinity(0)))  # threading on cpu only
+    torch.set_num_threads(8)  # threading on cpu only
+    print("Using CPU")
+
+device = torch.device(dev)
+mnist_vamp = {
+    "dataset_name": "mnist",
+    "prior": "vamp",
+    "pseudo_components": 500,
+    "warmup": 100,
+    "z1_size": 40,
+    "batch_size": 100,
+    "test_batch_size": 100,
+    "input_size": [1, 28, 28],
+    "input_type": "binary",
+    "pseudoinputs_std": 0.01,
+    "pseudoinputs_mean": 0.05,
+    "learning_rate": 0.0005,
+    "epochs": 30,
+    "pseudo_from_data": False,
+    "device": device,
+}
+
+mnist_standard = {
+    "dataset_name": "mnist",
+    "prior": "standard",
+    "pseudo_components": 500,
+    "warmup": 100,
+    "z1_size": 40,
+    "batch_size": 100,
+    "test_batch_size": 100,
+    "input_size": [1, 28, 28],
+    "input_type": "binary",
+    "pseudoinputs_std": 0.01,
+    "pseudoinputs_mean": 0.05,
+    "learning_rate": 0.0005,
+    "epochs": 30,
+    "pseudo_from_data": False,
+    "device": device,
+}
+
+
+
+mnist_mog = {
+    "dataset_name": "mnist",
+    "prior": "mog",
+    "pseudo_components": 500,
+    "warmup": 100,
+    "z1_size": 40,
+    "batch_size": 100,
+    "test_batch_size": 100,
+    "input_size": [1, 28, 28],
+    "input_type": "binary",
+    "pseudoinputs_std": 0.01,
+    "pseudoinputs_mean": 0.05,
+    "learning_rate": 0.0005,
+    "epochs": 30,
+    "pseudo_from_data": False,
+    "device": device,
+}
+
+
+frey_vamp = {
+    "dataset_name": "freyfaces",
+    "prior": "vamp",
+    "pseudo_components": 500,
+    "warmup": 100,
+    "z1_size": 40,
+    "batch_size": 100,
+    "input_size": [1, 28, 20],
+    "input_type": "cont",
+    "learning_rate": 0.0005,
+    "epochs": 30,
+    "pseudo_from_data": False,
+    "device": device,
+}
+
+frey__mog = {
+    "dataset_name": "freyfaces",
+    "prior": "mog",
+    "pseudo_components": 500,
+    "warmup": 100,
+    "z1_size": 40,
+    "batch_size": 100,
+    "input_size": [1, 28, 20],
+    "input_type": "cont",
+    "learning_rate": 0.0005,
+    "epochs": 30,
+    "pseudo_from_data": False,
+    "device": device,
+}
+
+
+frey_standard = {
+    "dataset_name": "freyfaces",
+    "prior": "standard",
+    "pseudo_components": 500,
+    "warmup": 100,
+    "z1_size": 40,
+    "batch_size": 100,
+    "input_size": [1, 28, 20],
+    "input_type": "cont",
+    "learning_rate": 0.0005,
+    "epochs": 30,
+    "pseudo_from_data": False,
+    "device": device,
+}
